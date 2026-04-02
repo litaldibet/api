a3bc984bc8ea352f6227b9cf6a8a8f55f28e457a
TiagoJSaraiva <saraivajtiago@gmail.com>
2026-04-01 23:17:18 -0300

    Configuração do cors para ser consumido apenas por aplicações do projeto litaldibet

--- supabase/lib/cors.ts
+++ supabase/lib/cors.ts
@@ -1,5 +1,7 @@
+const allowedOrigin = "https://litaldibet.github.io"
+
 export const corsHeaders = {
-  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Origin": allowedOrigin,
   "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS"
 }
